ffc62b9624802b214706502174607dbbb18252d8
hvyv-nnemo <58429516+hvyv-nnemo@users.noreply.github.com>
2020-11-19 17:13:57 +0900

    d

--- js/script_pay.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-    
-    setTimeout(function() {
-        console.log('WOW');
-        $('.payload').stop().animate({'left':'-100%'}, 600);
-        $('.qr_base').stop().animate({'left':'0'}, 300);
-      }, 3000);
-
-});
